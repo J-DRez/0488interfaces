793c8d5db99b3a469224ed6e7c4b233b69efe5b3
Julio Alberto Díaz Rodríguez <juliodr91@gmail.com>
2026-04-30 12:00:01 +0200

    TaskFlow Fase 5
    Terminado el proyecto hasta la fase 5 de la Actividad 3.

--- Actividad03/taskflow-landing/main.js
+++ Actividad03/taskflow-landing/main.js
@@ -1,24 +1,9 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const toggle = document.getElementById('theme-toggle');
+const iconMoon = document.getElementById('icon-moon');
+const iconSun = document.getElementById('icon-sun');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+toggle.addEventListener('click', () => {
+    document.documentElement.classList.toggle('dark');
+    iconMoon.classList.toggle('hidden');
+    iconSun.classList.toggle('hidden');
+});
